14f4b283190ef75e8529c49cc18d3e42f61a2853
NodirbekMatchanov <group.scala@mail.ru>
2022-04-04 23:12:27 +0500

    fix

--- views/support/user/_list.php
+++ views/support/user/_list.php
@@ -5,6 +5,6 @@ use yii\helpers\HtmlPurifier;
 
 <div class="news-item">
     <hr>
-    <a href="<?= 'support/categories?category='.Yii::$app->request->get('category').'&id='.$model->id?>"> <h4 class="question"><?= HtmlPurifier::process($model->question) ?></h4></a>
+    <a href="<?= '/web/support/categories?category='.Yii::$app->request->get('category').'&id='.$model->id?>"> <h4 class="question"><?= HtmlPurifier::process($model->question) ?></h4></a>
     <?= \app\components\CString::truncate(HtmlPurifier::process($model->answer),300) ?>
 </div>
