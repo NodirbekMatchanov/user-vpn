a271deaca7a027a09d0d883698c97e426c6d0701
NodirbekMatchanov <group.scala@mail.ru>
2022-04-06 08:40:12 +0500

    fix

---
+++ models/Payments.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "payments".
+ *
+ * @property int $id
+ * @property string $orderId
+ * @property int $user_id
+ * @property string $datecreate
+ * @property string $tariff
+ * @property float $amount
+ * @property int $status
+ */
+class Payments extends \yii\db\ActiveRecord
+{
+    const PAYED = 2;
+    const ERROR = 0;
+    const ERROR_BALANCE = 3;
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'payments';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['orderId', 'user_id', 'tariff', 'amount', 'status'], 'required'],
+            [[ 'user_id', 'status'], 'integer'],
+            [['datecreate'], 'safe'],
+            [['amount'], 'number'],
+            [['tariff','orderId'], 'string', 'max' => 50],
+            [['orderId'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'orderId' => 'Order ID',
+            'user_id' => 'User ID',
+            'datecreate' => 'Datecreate',
+            'tariff' => 'Tariff',
+            'amount' => 'Amount',
+            'status' => 'Status',
+        ];
+    }
+}
