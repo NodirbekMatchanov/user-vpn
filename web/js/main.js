9f7d63cad22d8111d70f5b1764344fec5e18ae2e
NodirbekMatchanov <group.scala@mail.ru>
2022-04-13 21:28:44 +0500

    fix

--- web/js/main.js
+++ web/js/main.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
-    $('#kt_repeater_1').repeater({
-        initEmpty: false,
+    if($('#kt_repeater_1').length){
+        $('#kt_repeater_1').repeater({
+            initEmpty: false,
 
-        defaultValues: {
-            'text-input': 'foo'
-        },
+            defaultValues: {
+                'text-input': 'foo'
+            },
 
-        show: function () {
-            $(this).slideDown();
-        },
+            show: function () {
+                $(this).slideDown();
+            },
 
-        hide: function (deleteElement) {
-            $(this).slideUp(deleteElement);
-        }
-    });
+            hide: function (deleteElement) {
+                $(this).slideUp(deleteElement);
+            }
+        });
+    }
 })
