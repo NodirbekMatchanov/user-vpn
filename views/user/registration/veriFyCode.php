f0f5e4bfeb33ecd2e4b312b6ad64dd98323535ac
NodirbekMatchanov <group.scala@mail.ru>
2022-04-20 20:32:14 +0500

    fix

--- views/user/registration/veriFyCode.php
+++ views/user/registration/veriFyCode.php
@@ -5,19 +5,6 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 ?>
-<?php if ($module->enableFlashMessages): ?>
-    <div class="row">
-        <div class="col-xs-12">
-            <?php foreach (Yii::$app->session->getAllFlashes() as $type => $message): ?>
-                <?php if (in_array($type, ['success', 'danger', 'warning', 'info'])): ?>
-                <div class="alert alert-<?=$type?>">
-                    <?=$message?>
-                </div>
-                <?php endif ?>
-            <?php endforeach ?>
-        </div>
-    </div>
-<?php endif ?>
 <div class="row">
 
     <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
