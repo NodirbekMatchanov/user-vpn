b0cc3f2ba87100d4cdf1dd28e15a0bf509edf63f
NodirbekMatchanov <group.scala@mail.ru>
2022-06-14 16:02:24 +0500

    fix

--- views/user/mail/_parts/footer.php
+++ views/user/mail/_parts/footer.php
@@ -1,41 +1,43 @@
+<?php $root = 'https://www.vpn-max.com/web/' ?>
+<?php $class = '@@class' ?>
 
 <div class="footer">
-	<div class="socials">
+    <div class="socials">
 
-		<?php if ($class == '_dark'): ?>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-blue-inst.png" alt="">
-			</a>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-blue-fb.png" alt="">
-			</a>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-blue-tw.png" alt="">
-			</a>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-blue-youtube.png" alt="">
-			</a>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-blue-tg.png" alt="">
-			</a>
-		<?php else: ?>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-inst.png" alt="">
-			</a>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-fb.png" alt="">
-			</a>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-tw.png" alt="">
-			</a>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-youtube.png" alt="">
-			</a>
-			<a href="#" class="socials-item">
-				<img src="<?= $root ?>/img/icons/email-socials-telegram.png" alt="">
-			</a>
-		<?php endif; ?>
-	</div>
+        <?php if ($class == '_dark'): ?>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-blue-inst.png" alt="">
+            </a>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-blue-fb.png" alt="">
+            </a>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-blue-tw.png" alt="">
+            </a>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-blue-youtube.png" alt="">
+            </a>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-blue-tg.png" alt="">
+            </a>
+        <?php else: ?>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-inst.png" alt="">
+            </a>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-fb.png" alt="">
+            </a>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-tw.png" alt="">
+            </a>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-youtube.png" alt="">
+            </a>
+            <a href="#" class="socials-item">
+                <img src="<?= $root ?>/img/icons/email-socials-telegram.png" alt="">
+            </a>
+        <?php endif; ?>
+    </div>
 </div>
 
 </div>
--- views/user/mail/_parts/header.php
+++ views/user/mail/_parts/header.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <html lang="ru">
+<?php $root = 'https://www.vpn-max.com/web/' ?>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../css/email.css?<?= $app->hash ?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=$root?>/tmp/public/css/email.css?<?= time() ?>">
 </head>
 <?php $class = '@@class' ?>
-<?php $root = 'https://vpnmax.webresult.online/' ?>
-
 <body class="@@class">
-	<!-- все картинки должны быть с полным путем к сайту -->
+<!-- все картинки должны быть с полным путем к сайту -->
 
-	<div class="wrap">
+<div class="wrap">
 
-		<div class="header">
-	<div class="header-logo">
-		<!-- только png/jpg -->
-		<img src="<?= $root ?>/img/logo.png" alt="">
-	</div>
-</div>
+    <div class="header">
+        <div class="header-logo">
+            <!-- только png/jpg -->
+            <img src="<?= $root ?>/img/logo.png" alt="">
+        </div>
+    </div>
