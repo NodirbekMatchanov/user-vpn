bc3b389d29aacebb24c33d6162bddc8ee23a7cf5
NodirbekMatchanov <group.scala@mail.ru>
2022-06-14 16:20:36 +0500

    fix

--- views/user/mail/layouts/html.php
+++ views/user/mail/layouts/html.php
@@ -23,6 +23,133 @@
     <link rel="stylesheet" href="<?=$root?>/tmp/public/css/email.css?<?= time() ?>">
 </head>
 <?php $class = '@@class' ?>
+<style>
+    main > .container {
+        padding: 70px 15px 20px;
+    }
+
+    .footer {
+        background-color: #f5f5f5;
+        font-size: .9em;
+        height: 60px;
+    }
+    .question {
+        color: #56b366;
+    }
+
+    .NoActiveStatus {
+        background:   #B9B9B9;
+    }
+    .DeletedStatus {
+        background: #FFC4C4 ;
+    }
+    .ActiveStatus {
+        background: #def7d3;
+    }
+
+    html, body{
+        height: 100%;
+    }
+    .wrap_ {
+        min-height: 100%;
+        height: auto;
+        padding: 0 0 60px;
+    }
+    .clearfix:before, .clearfix:after, .nav:before, .nav:after, .navbar:before, .navbar:after, .navbar-header:before, .navbar-header:after, .navbar-collapse:before, .navbar-collapse:after{
+        display:none;
+    }
+    .navbar .container, .navbar .container-fluid, .navbar .container-sm, .navbar .container-md, .navbar .container-lg, .navbar .container-xl{
+        display: block!important;
+    }
+    body {
+        font-size: 14px!important;
+        line-height: 1.42857143!important;
+    }
+    .footer > .container {
+        padding-right: 15px;
+        padding-left: 15px;
+    }
+    .navbar-nav{
+        flex-direction:row!important;
+    }
+    .navbar-brand {
+        float: left!important;
+        height: 50px!important;
+        padding: 15px 15px!important;
+        font-size: 18px!important;
+        line-height: 20px!important;
+    }
+    .not-set {
+        color: #c55;
+        font-style: italic;
+    }
+
+    /* add sorting icons to gridview sort links */
+    a.asc:after, a.desc:after {
+        content: '';
+        left: 3px;
+        display: inline-block;
+        width: 0;
+        height: 0;
+        border: solid 5px transparent;
+        margin: 4px 4px 2px 4px;
+        background: transparent;
+    }
+
+    a.asc:after {
+        border-bottom: solid 7px #212529;
+        border-top-width: 0;
+    }
+
+    a.desc:after {
+        border-top: solid 7px #212529;
+        border-bottom-width: 0;
+    }
+
+    .grid-view th {
+        white-space: nowrap_;
+    }
+
+    .hint-block {
+        display: block;
+        margin-top: 5px;
+        color: #999;
+    }
+
+    .error-summary {
+        color: #a94442;
+        background: #fdf7f7;
+        border-left: 3px solid #eed3d7;
+        padding: 10px 20px;
+        margin: 0 0 15px 0;
+    }
+
+    /* align the logout "link" (button in form) of the navbar */
+    .nav li > form > button.logout {
+        padding-top: 7px;
+        color: rgba(255, 255, 255, 0.5);
+    }
+
+    @media(max-width:767px) {
+        .nav li > form > button.logout {
+            display:block;
+            text-align: left;
+            width: 100%;
+            padding: 10px 0;
+        }
+    }
+
+    .nav > li > form > button.logout:focus,
+    .nav > li > form > button.logout:hover {
+        text-decoration: none;
+        color: rgba(255, 255, 255, 0.75);
+    }
+
+    .nav > li > form > button.logout:focus {
+        outline: none;
+    }
+
+</style>
 <body class="@@class">
 <!-- все картинки должны быть с полным путем к сайту -->
 
--- views/user/mail/welcome.php
+++ views/user/mail/welcome.php
@@ -20,7 +20,6 @@ use yii\helpers\Html;
 $template = \app\models\MailTemplate::find()->where(['tmp_key' => 'welcome'])->one();
 
 ?>
-<?php echo $this->render('_parts/header.php', []); ?>
 
 <?php if(empty($template)): ?>
     <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
@@ -62,4 +61,3 @@ $template = \app\models\MailTemplate::find()->where(['tmp_key' => 'welcome'])->o
     <?= $templateStr?>
 <?php endif;?>
 
-<?php echo $this->render('_parts/footer.php', []); ?>
