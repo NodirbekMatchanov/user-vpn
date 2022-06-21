69bda3bdf4ff9c56edb86ef71238532e61078771
NodirbekMatchanov <group.scala@mail.ru>
2022-06-21 17:37:10 +0500

    fix

--- views/support/user/index.php
+++ views/support/user/index.php
@@ -146,9 +146,6 @@ if($category = Yii::$app->request->get('category')){
 
 
 </style>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <div class="container" style="margin-top: 50px">
     <form action="/ru/" autocomplete="off" class="header__form search" style="background: whitesmoke">
         <input type="text" autocomplete="off" class="search__input js__search-input o__ltr" placeholder="Поиск ответов..." tabindex="1" name="q" value="">
