d815278a6a4253b86a14a323e77f24f4b34e7f30
NodirbekMatchanov <group.scala@mail.ru>
2022-07-05 09:54:36 +0500

    fix

--- views/user/settings/account.php
+++ views/user/settings/account.php
@@ -29,6 +29,18 @@ $this->params['breadcrumbs'][] = $this->title;
         width: 100%;
     }
 </style>
+<div class="row">
+    <div class="col-xs-12">
+        <?php foreach (Yii::$app->session->getAllFlashes() as $type => $message): ?>
+            <?php if (in_array($type, ['success', 'danger', 'warning', 'info'])): ?>
+                <?= \app\widgets\Alert::widget([
+                    'options' => ['class' => 'alert-dismissible alert-' . $type],
+                    'body' => $message
+                ]) ?>
+            <?php endif ?>
+        <?php endforeach ?>
+    </div>
+</div>
 <div class="settings">
     <div class="container">
         <h3 class="title-3">Имя пользователя OpenVPN / IKEv2</h3>
