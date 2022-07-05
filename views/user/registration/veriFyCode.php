b5b6d92713de634e8042410c3b34db5aaf09a44d
NodirbekMatchanov <group.scala@mail.ru>
2022-07-05 10:27:54 +0500

    fix

--- views/user/registration/veriFyCode.php
+++ views/user/registration/veriFyCode.php
@@ -5,19 +5,19 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 ?>
-<div class="row">
-    <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+<div class="container">
+    <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3" style="padding: 100px">
         <h3>Подтверждения код активации</h3>
             <?php $form = ActiveForm::begin([
                 'id' => 'registration-form',
             ]); ?>
 
 
-        <div class="input-2">
+        <div class="input-2" style="margin: 15px">
             <label for="" class="input-2-label">Code</label>
             <?= $form->field($verifyCode, 'code')->label(false) ?>
         </div>
-        <?= Html::submitButton('Активировать', ['class' => 'btn-2 _outline']) ?>
+        <?= Html::submitButton('Активировать', ['class' => 'btn-2 _outline','style' => 'margin-top:20px']) ?>
 
             <?php ActiveForm::end(); ?>
 
