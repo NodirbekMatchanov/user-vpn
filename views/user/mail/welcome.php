8ebb483211e6b9f6b17bb4346b49bba7a4afa1b1
NodirbekMatchanov <group.scala@mail.ru>
2022-07-08 18:34:02 +0500

    fix

--- views/user/mail/welcome.php
+++ views/user/mail/welcome.php
@@ -21,7 +21,7 @@ $template = \app\models\MailTemplate::find()->where(['tmp_key' => 'welcome'])->o
 $accs = \app\models\Accs::find()->where(['email' => $user->email])->one();
 
 ?>
-<?php if ($accs->status == \app\models\VpnUserSettings::$statuses['ACTIVE']): ?>
+<?php if ($user->module->enableConfirmation == false): ?>
     <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
         <?= Yii::t('user', 'Hello') ?>,
     </p>
