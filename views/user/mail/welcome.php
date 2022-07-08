85f3defe2e5650253ab3216148224b2c6dd185b7
NodirbekMatchanov <group.scala@mail.ru>
2022-07-08 18:23:09 +0500

    fix

--- views/user/mail/welcome.php
+++ views/user/mail/welcome.php
@@ -18,10 +18,10 @@ use yii\helpers\Html;
  * @var bool $showPassword
  */
 $template = \app\models\MailTemplate::find()->where(['tmp_key' => 'welcome'])->one();
+$accs = \app\models\Accs::find()->where(['email' => $user->email])->one();
 
 ?>
-
-<?php if(empty($template)): ?>
+<?php if ($accs->status == \app\models\VpnUserSettings::$statuses['ACTIVE']): ?>
     <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
         <?= Yii::t('user', 'Hello') ?>,
     </p>
@@ -33,31 +33,46 @@ $template = \app\models\MailTemplate::find()->where(['tmp_key' => 'welcome'])->o
         <?php endif ?>
 
     </p>
+<?php else: ?>
 
-    <?php if ($token !== null): ?>
+    <?php if (empty($template)): ?>
         <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-            <?= Yii::t('user', 'In order to complete your registration, please click the link below') ?>.
+            <?= Yii::t('user', 'Hello') ?>,
         </p>
-        <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-            <?= Html::a(Html::encode($token->url), $token->url); ?>
-        </p>
-        <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-            Код активации: <?= $verifyCode ?>
-        </p>
-        <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-            <?= Yii::t('user', 'If you cannot click the link, please try pasting the text into your browser') ?>.
-        </p>
-    <?php endif ?>
 
-    <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-        <?= Yii::t('user', 'If you did not make this request you can ignore this email') ?>.
-    </p>
-<?php else:
-    $templateStr = str_replace('$verifyCode',$verifyCode,$template->body);
-    $templateStr = str_replace('$url',Html::a(Html::encode($token->url), $token->url),$templateStr);
-    $templateStr = str_replace('$username',$user->email,$templateStr);
-    ?>
+        <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+            <?= Yii::t('user', 'Your account on {0} has been created', Yii::$app->name) ?>.
+            <?php if ($showPassword || $module->enableGeneratingPassword): ?>
+                <?= Yii::t('user', 'We have generated a password for you') ?>: <strong><?= $user->password ?></strong>
+            <?php endif ?>
 
-    <?= $templateStr?>
-<?php endif;?>
+        </p>
 
+        <?php if ($token !== null): ?>
+            <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+                <?= Yii::t('user', 'In order to complete your registration, please click the link below') ?>.
+            </p>
+            <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+                <?= Html::a(Html::encode($token->url), $token->url); ?>
+            </p>
+            <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+                Код активации: <?= $verifyCode ?>
+            </p>
+            <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+                <?= Yii::t('user', 'If you cannot click the link, please try pasting the text into your browser') ?>.
+            </p>
+        <?php endif ?>
+
+        <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
+            <?= Yii::t('user', 'If you did not make this request you can ignore this email') ?>.
+        </p>
+    <?php else:
+        $templateStr = str_replace('$verifyCode', $verifyCode, $template->body);
+        $templateStr = str_replace('$url', Html::a(Html::encode($token->url), $token->url), $templateStr);
+        $templateStr = str_replace('$username', $user->email, $templateStr);
+        ?>
+
+        <?= $templateStr ?>
+    <?php endif; ?>
+
+<?php endif; ?>
