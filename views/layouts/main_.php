3da4b6d99b73c98f182f180e90754261baffd988
Rare <group.scala@mail.ru>
2022-11-01 23:56:20 +0500

    update

--- views/layouts/main_.php
+++ views/layouts/main_.php
@@ -28,13 +28,22 @@ use yii\bootstrap\NavBar;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
           id="metaViewport">
-    <meta name="twitter:title" content="VPN MAX">
-    <meta property="og:title" content="VPN MAX">
+    <meta name="twitter:title" content="VPNMAX - Быстрый и анонимный доступ к любым сайтам через VPN">
+    <meta property="og:title" content="VPNMAX - Быстрый и анонимный доступ к любым сайтам через VPN">
+
     <meta name="description" content="">
     <meta property="og:description" content="">
-    <meta property="og:image" content="/web/img/default.png">
-    <meta property="vk:image" content="/web/img/default.png">
-    <meta name="twitter:image" content="/web/img/default.png">
+    <meta property="og:image" content="img/logo.png">
+    <meta property="vk:image" content="img/logo.png">
+    <meta name="twitter:image" content="img/logo.png">
+    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#00bdd7">
+    <meta name="msapplication-TileColor" content="#008CF2">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="shortcut icon" href="/web/img/favicon/favicon.ico" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
